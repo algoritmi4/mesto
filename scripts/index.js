dc100acd66efcc33a01b31e51de694f2dee9e206
Ivan <ButorinLP@yandex.ru>
2023-02-20 19:28:39 +0300

    Фикс после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,30 @@ const profilePopup = document.querySelector('#profile-popup');
 
 const profilePopupForm = document.querySelector('#profile-popup-form');
 
+const cardTemplate = document.querySelector('#new-card').content.querySelector('.photo-grid__card');
+
+const picturePopup = document.querySelector('#picture-popup');
+  
+const picturePopupImage = document.querySelector('.popup__image');
+
+const picturePopupCaption = document.querySelector('.popup__caption');
+
+const cardPopup = document.querySelector('#card-popup');
+
+const cardPopupForm = document.querySelector('#card-popup-form');
+
+const userName = document.querySelector('.profile__text_type_name');
+
+const userActivity = document.querySelector('.profile__text_type_activity');
+
+const userInputName = document.querySelector('#popup__input_type_name');
+
+const userInputActivity = document.querySelector('#popup__input_type_activity');
+
+const userInputTitle = document.querySelector('#card-popup__input_type_title');
+
+const userInputPicture = document.querySelector('#card-popup__input_type_picture');
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -38,17 +62,19 @@ const initialCards = [
 // Открытие попапа
 const openPopup = (popup) => {
   popup.classList.add('popup_opened');
+
+  document.addEventListener('keydown', closePopupEscapePressed);
 };
 
 // Закрытие попапа
 const closePopup = (popup) => {
   popup.classList.remove('popup_opened');
+
+  document.removeEventListener('keydown', closePopupEscapePressed);
 };
 
 // Создание карточки и навешивание на нее всех слушателей
 const createCard = (item) => {
-  const cardTemplate = document.querySelector('#new-card').content.querySelector('.photo-grid__card');
-
   const cardElement = cardTemplate.cloneNode(true);
 
   const newCardLike = cardElement.querySelector('.photo-grid__heart');
@@ -58,12 +84,6 @@ const createCard = (item) => {
   const cardElementImage = cardElement.querySelector('.photo-grid__image');
 
   const cardElementTitle = cardElement.querySelector('.photo-grid__card-title');
-
-  const picturePopup = document.querySelector('#picture-popup');
-
-  const picturePopupImage = document.querySelector('.popup__image');
-  
-  const picturePopupCaption = document.querySelector('.popup__caption');
 
   cardElementImage.src = item.link;
   cardElementImage.alt = item.name;
@@ -78,8 +98,6 @@ const createCard = (item) => {
     picturePopupImage.src = cardElementImage.src;
     picturePopupImage.alt = cardElementImage.alt;
     picturePopupCaption.textContent = cardElementTitle.textContent;
-
-    setEscapeEventListener();
   });
 
   return cardElement;
@@ -96,82 +114,59 @@ const addCard = (item) => {
 
 // Закрытие попапа при нажатии клавиши "Escape"
 const closePopupEscapePressed = (e) => {
-  const popupOpened = document.querySelector('.popup_opened');
-  
   if(e.key === 'Escape') {
-    closePopup(popupOpened);
+    const popupOpened = document.querySelector('.popup_opened');
 
-    document.removeEventListener('keydown', closePopupEscapePressed);
+    closePopup(popupOpened);
   };
 };
 
-// Добавление слушателя вынесено в отдельную ф-ию
-// т.к. требуется в нескольких местах
-const setEscapeEventListener = () => {
-  document.addEventListener('keydown', closePopupEscapePressed);
-};
+profilePopupForm.addEventListener('submit', () => {
+  closePopup(profilePopup);
+  userName.textContent = userInputName.value;
+  userActivity.textContent = userInputActivity.value;
+});
 
-const setEventListeners = () => {
-  const cardPopup = document.querySelector('#card-popup');
+cardPopupForm.addEventListener('submit', (e) => {
+  closePopup(cardPopup);
 
-  const cardPopupForm = document.querySelector('#card-popup-form');
-  
-  const userName = document.querySelector('.profile__text_type_name');
-  
-  const userActivity = document.querySelector('.profile__text_type_activity');
-  
-  const userInputName = document.querySelector('#popup__input_type_name');
-  
-  const userInputActivity = document.querySelector('#popup__input_type_activity');
-  
-  const userInputTitle = document.querySelector('#card-popup__input_type_title');
-  
-  const userInputPicture = document.querySelector('#card-popup__input_type_picture');
-
-  profilePopupForm.addEventListener('submit', () => {
-    closePopup(profilePopup);
-    userName.textContent = userInputName.value;
-    userActivity.textContent = userInputActivity.value;
+  addCard({
+    name: userInputTitle.value,
+    link: userInputPicture.value
   });
-  
-  cardPopupForm.addEventListener('submit', (e) => {
-    closePopup(cardPopup);
 
-    addCard({
-      name: userInputTitle.value,
-      link: userInputPicture.value
-    });
+  e.target.reset();
+});
 
-    e.target.reset();
-  });
-  
-  editButton.addEventListener('click', () => {
-    openPopup(profilePopup);
-    userInputName.value = userName.textContent;
-    userInputActivity.value = userActivity.textContent;
+editButton.addEventListener('click', () => {
+  openPopup(profilePopup);
+  userInputName.value = userName.textContent;
+  userInputActivity.value = userActivity.textContent;
+});
 
-    setEscapeEventListener();
-  });
-  
-  addCardButton.addEventListener('click', () => {
-    openPopup(cardPopup);
+addCardButton.addEventListener('click', () => {
+  openPopup(cardPopup);
 
-    setEscapeEventListener();
-  });
-};
+  const cardPopupInputTitle = cardPopup.querySelector('#card-popup__input_type_title');
 
+  if(cardPopupInputTitle.value === '') {
+    const cardPopupButton = cardPopup.querySelector('#card-popup__button');
+
+    cardPopupButton.disabled = true;
+    cardPopupButton.classList.add('popup__button_disabled');
+  };
+});
+
+// Добавление карточек при загрузке страницы
 initialCards.forEach(addCard);
 
+// Закрытие попапов при клике по оверлею
 popups.forEach(popup => {
   popup.addEventListener('click', e => {
     const closeButton = popup.querySelector('.popup__cross')
 
     if(e.target === e.currentTarget || e.target === closeButton) {
       closePopup(popup);
-
-      document.removeEventListener('keydown', closePopupEscapePressed);
     };
   });
 });
-
-setEventListeners();
