d33f86ad5380fb471911316e7822ce3048d8cd49
Ivan <ButorinLP@yandex.ru>
2023-03-24 22:28:41 +0300

    8/fixed

--- webpack.config.js
+++ webpack.config.js
@@ -32,8 +32,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        }]
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        },
+        'postcss-loader']
       }
     ]
   },
@@ -43,5 +45,5 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin()
-  ] 
+  ],
 }
